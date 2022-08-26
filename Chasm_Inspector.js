77bd6e82e1a82507633509c113b854161ab0e94d
Jason Brown <jason.brown@masimo.com>
2022-08-25 18:20:38 -0700

    Inspector layout improvements

--- Chasm_Inspector.js
+++ Chasm_Inspector.js
@@ -8,6 +8,12 @@
 
 class _INSPECTOR_ID {
 	none 				= 0x0000;		// 0x0000 Clear inspector
+
+	particles			= 0xa001;		// 0xa0xx Start currency section
+	strands				= 0xa002;
+	spirit				= 0xa003;
+	soul				= 0xa004;
+
 	steel_toed_boots 	= 0xa101;		// 0xa1xx Start upgrade section
 	ant_farm			= 0xa102;
 	catapult			= 0xa103;
@@ -17,54 +23,88 @@ class _INSPECTOR_ID {
 } var iid = new _INSPECTOR_ID();
 
 function registerInspectorEvents() {
-	$("#upgrade_steel_toed_boots")	.mouseenter(function(){showInspector(iid.steel_toed_boots);});
-	$("#upgrade_ant_farm")			.mouseenter(function(){showInspector(iid.ant_farm);});
-	$("#upgrade_catapult")			.mouseenter(function(){showInspector(iid.catapult);});
-	$("#upgrade_water_storage")		.mouseenter(function(){showInspector(iid.water_storage);});
-	$("#upgrade_rain_barrels")		.mouseenter(function(){showInspector(iid.rain_barrels);});
-	$("#upgrade_sprinkler")			.mouseenter(function(){showInspector(iid.sprinkler);});
+	$(".currency_particles")				.mouseenter(function(){showInspector(iid.particles);});
+	$(".currency_strands")					.mouseenter(function(){showInspector(iid.strands);});
+	$(".currency_spirit")					.mouseenter(function(){showInspector(iid.spirit);});
+	$(".currency_soul")						.mouseenter(function(){showInspector(iid.soul);});
+
+	$("#select_upgrade_steel_toed_boots")	.mouseenter(function(){showInspector(iid.steel_toed_boots);});
+	$("#select_upgrade_ant_farm")			.mouseenter(function(){showInspector(iid.ant_farm);});
+	$("#select_upgrade_catapult")			.mouseenter(function(){showInspector(iid.catapult);});
+	$("#select_upgrade_water_storage")		.mouseenter(function(){showInspector(iid.water_storage);});
+	$("#select_upgrade_rain_barrels")		.mouseenter(function(){showInspector(iid.rain_barrels);});
+	$("#select_upgrade_sprinkler")			.mouseenter(function(){showInspector(iid.sprinkler);});
 }
 
 var current_inspector_id;
 
 function showInspector(id) {
 	switch(id) {
+		// Currency
+		case iid.particles:
+			$("#inspector_title")	.html("Void Particles");
+			$("#inspector_cost")	.html("");
+			$("#inspector_text")	.html("When matter is dropped into the Chasm, it releases small clouds of nothing. Our researchers say this is supposed to be impossible.");
+			$("#inspector_subtext")	.html("");
+			break;
+		case iid.strands:
+			$("#inspector_title")	.html("Gravity Strands");
+			$("#inspector_cost")	.html("");
+			$("#inspector_text")	.html("When dense matter is dropped into the Chasm, it releases gossamer strands of gravity. Our researchers say gravity has no carrying particle, but here it is.");
+			$("#inspector_subtext")	.html("");
+			break;
+		case iid.spirit:
+			$("#inspector_title")	.html("Spirit Sand");
+			$("#inspector_cost")	.html("");
+			$("#inspector_text")	.html("When living matter is dropped into the Chasm, it sprays grains of silver-green sand. Our researchers are conivinced this sand has something to do with a metaphysical 'life-force'.");
+			$("#inspector_subtext")	.html("");
+			break;
+		case iid.soul:
+			$("#inspector_title")	.html("Soul Shards");
+			$("#inspector_cost")	.html("");
+			$("#inspector_text")	.html("When human flesh is dropped into the Chasm, it screams. Glassy shards grow around the edge of the pit... It's best not to consider what they are made of.");
+			$("#inspector_subtext")	.html("");
+			break;
+		
+		// Upgrades
 		case iid.steel_toed_boots:
 			$("#inspector_title")	.html("Steel-toed Boots");
-			$("#inspector_text")	.html("You can fit a lot more dirt into your storage with a few well-placed stomps");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.steel_toed_boots].cost.stringify());
+			$("#inspector_text")	.html("You can fit a lot more dirt into your storage with a few well-placed stomps");
 			$("#inspector_subtext")	.html("2x Earth density");
 			break;
 		case iid.ant_farm:
 			$("#inspector_title")	.html("Ant farm");
-			$("#inspector_text")	.html("These little guys can help you move mountains of earth... Very, very slowly");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.ant_farm].cost.stringify());
+			$("#inspector_text")	.html("These little guys can help you move mountains of earth... Very, very slowly");
 			$("#inspector_subtext")	.html("Auto-gather Earth");
 			break;
 		case iid.catapult:
 			$("#inspector_title")	.html("Catapult");
-			$("#inspector_text")	.html("Flinging dirt into the Chasm is a lot more fun than dumping it in by hand");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.catapult].cost.stringify());
+			$("#inspector_text")	.html("Flinging dirt into the Chasm is a lot more fun than dumping it in by hand");
 			$("#inspector_subtext")	.html("Auto-drop Earth");
 			break;
 		case iid.water_storage:
 			$("#inspector_title")	.html("Water storage");
-			$("#inspector_text")	.html("Dumping water into the Chasm might speed things up, but you'll have to build some water tanks first");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.water_storage].cost.stringify());
+			$("#inspector_text")	.html("Dumping water into the Chasm might speed things up, but you'll have to build some water tanks first");
 			$("#inspector_subtext")	.html("Unlock Water");
 			break;
 		case iid.rain_barrels:
 			$("#inspector_title")	.html("Rain barrels");
-			$("#inspector_text")	.html("Your back hurts from carrying so much water. Let mother nature do some of the work herself");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.rain_barrels].cost.stringify());
+			$("#inspector_text")	.html("Your back hurts from carrying so much water. Let mother nature do some of the work herself");
 			$("#inspector_subtext")	.html("Auto-gather Water");
 			break;
 		case iid.sprinkler:
 			$("#inspector_title")	.html("Sprinkler");
-			$("#inspector_text")	.html("Attach a sprinkler system to your water tank to spray directly into the Chasm");
 			$("#inspector_cost")	.html(chasm_upgrades[uid.sprinkler].cost.stringify());
+			$("#inspector_text")	.html("Attach a sprinkler system to your water tank to spray directly into the Chasm");
 			$("#inspector_subtext")	.html("Auto-drop Water");
 			break;
+
+		// Clear
 		case iid.none:
 		default:
 			$("#inspector_title")	.html("");

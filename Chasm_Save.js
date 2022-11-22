e087a22900177cc4abd6852be457ab16a25d5a08
Jason Brown <jason.brown@masimo.com>
2022-11-21 16:37:30 -0800

    Moving save functions to lib

--- Chasm_Save.js
+++ Chasm_Save.js
@@ -1,8 +1,5 @@
 // Game Save data
 // Need a way to handle save data version change
-chasm_version_major = 0;
-chasm_version_minor = 1;
-
 class saveData {
 	version_major;
 	version_minor;
@@ -11,34 +8,31 @@ class saveData {
 	//achievements = chasm_achievements;
 
 	constructor() {
-		this.version_major = chasm_version_major;
-		this.version_minor = chasm_version_minor;
+		this.version_major = 0;
+		this.version_minor = 1;
 		this.saveCount = 0;
 	}
 }
 
 var chasm_save;
 var chasm_incoming_save;
+const save_path = "chasm";
 
 function loadSave() {
 	chasm_save = new saveData();
-	chasm_incoming_save = JSON.parse(localStorage.getItem("chasm"));
+	chasm_incoming_save = lib_chasm_load_save(save_path);
 	if (!chasm_incoming_save) {
 		// New Game
 	} else {
 		// Load Game
-		for (var prop in chasm_incoming_save) {
-			if (!chasm_save.hasOwnProperty(prop)) {
-				delete chasm_incoming_save[prop];
-			}
-		}
-		Object.assign(chasm_save, chasm_incoming_save);
+		lib_chasm_match_save_classes(chasm_save, chasm_incoming_save);
+		lib_chasm_merge_save(chasm_save, chasm_incoming_save);
 	}
 }
 
 function storeSave() {
 	chasm_save.saveCount++;
-	localStorage.setItem("chasm", JSON.stringify(chasm_save));
+	lib_chasm_store_save(save_path, chasm_save);
 }
 
 function autoSave() {
